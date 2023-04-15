eb75075d7a534494a529677b3089b3b0712d196f
chouvel <3094774079@qq.com>
2023-04-15 15:14:42 +0800

    在代码中添加了描述的一句话

---
+++ 判断年份.py
@@ -0,0 +1,15 @@
+while True:
+    year_str = input("请输入一个年份：")
+    if not year_str.isdigit():
+        print("输入的不是整数，请重新输入")
+        continue
+    year = int(year_str)
+    if year <= 0:
+        print("输入的年份必须大于 0，请重新输入")
+        continue
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        print(year, "是闰年")
+    else:
+        print(year, "是平年")
+    break
+这是一个控制台上的用来判断是否为平年或闰年的Python程序。
