71da835c9a61fdfd8f1fc14b20f2a8272ff2c108
chouvel <3094774079@qq.com>
2023-04-30 11:28:41 +0800

    笔记记录过程中的代码

---
+++ 字节青训营文件/test.js
@@ -0,0 +1,23 @@
+//闭包练习.
+
+
+// function showName(){
+//     const name='张三';
+//     const age=23;
+//     console.log('name');
+//     return function (){
+//         console.log(age);
+//         return name;
+//     }
+
+
+// };
+// const ageName=showName();
+// console.log(ageName());
+
+//this指向问题.
+
+// function printthis(){
+//     console.log(this)
+// };
+// printthis()
